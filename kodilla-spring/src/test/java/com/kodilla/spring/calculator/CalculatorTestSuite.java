9bca3788f8a569486146b2309736dea61bfdbd8b
Mateusz Wójciak <matwojciak@gmail.com>
2020-11-14 00:50:33 +0100

    14.2 Bean - calculator v1

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,8 +1,11 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 class CalculatorTestSuite {
@@ -14,34 +17,38 @@ class CalculatorTestSuite {
     void testAddition() {
         //Given
         //When
-        calculator.add(5, 5);
+        double result = calculator.add(5, 5);
 
         //Then
+        Assertions.assertEquals(10, result);
     }
 
     @Test
     void testSubtraction() {
         //Given
         //When
-        calculator.sub(100, 25);
+        double result = calculator.sub(100, 25);
 
         //Then
+        Assertions.assertEquals(75, result);
     }
 
     @Test
     void testMultiplication() {
         //Given
         //When
-        calculator.mul(15, 4);
+        double result = calculator.mul(15, 4);
 
         //Then
+        Assertions.assertEquals(60, result);
     }
     @Test
     void testDivision() {
         //Given
         //When
-        calculator.div(21, 7);
+        double result = calculator.div(21, 7);
 
         //Then
+        Assertions.assertEquals(3, result);
     }
 }
