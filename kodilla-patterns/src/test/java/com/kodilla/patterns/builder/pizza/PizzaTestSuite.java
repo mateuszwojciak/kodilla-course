7beb85623b96a6d3dbed0c6906ab73e7acf814e4
Mateusz Wójciak <matwojciak@gmail.com>
2020-11-20 13:31:22 +0100

    15.6 Update_ Debugger

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/pizza/PizzaTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/pizza/PizzaTestSuite.java
@@ -2,7 +2,7 @@ package com.kodilla.patterns.builder.pizza;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 class PizzaTestSuite {
 
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,20 @@
+package com.kodilla.testing.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class testLoopAndConditionalBreakpoints {
+    @Test
+    void testLoop() {
+        //Given
+        long sum = 0;
+        //When
+        for(int i = 0; i < 1000; i++) {
+            sum += i;
+            System.out.println("[" + i + "] Sum equals: " + sum);
+        }
+        //Then
+        assertEquals(499500, sum);
+    }
+}
